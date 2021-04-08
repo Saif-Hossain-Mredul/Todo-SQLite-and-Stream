52573ce4d4892cb6a112253c1f2404689a5cff0b
Saif Hossain <atonlymesaif@gmail.com>
2021-04-08 20:08:35 +0600

    Home screen added

--- lib/screens/home-screens.Screen.dart
+++ lib/screens/home-screens.Screen.dart
@@ -8,6 +8,57 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 40,
+        ),
+        onPressed: () {},
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(left: 5, right: 5, top: 60, bottom: 5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Text(
+                  'My tasks',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Text(
+                  '1 of 10',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('Task Name'),
+                      subtitle: Text('2 Days ago'),
+                      trailing: Checkbox(
+                        value: true,
+                        onChanged: (newVal) {},
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
