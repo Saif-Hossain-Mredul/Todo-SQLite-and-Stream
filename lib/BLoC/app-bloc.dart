3d13ab499ea9134f13a17e69b87dbe3cff8dbb80
Saif Hossain <atonlymesaif@gmail.com>
2021-04-14 13:15:39 +0600

    BLoC event re-organized.

--- lib/BLoC/app-bloc.dart
+++ lib/BLoC/app-bloc.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:my_app_part1_and_part2/services/sql/database-helper.service.dart';
-import 'package:my_app_part1_and_part2/utilities/bloc-model.utilities.dart';
+import 'file:///F:/Flutter/My%20Works/Todo-SQLite-and-BLoC/lib/BLoC/bloc-model.utilities.dart';
 
 class DataBloc with ChangeNotifier {
   final _dataBase = DatabaseHelper.instance;
